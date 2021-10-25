a6998d2fdf030825a232f92ad62dc1e9842920e5
oleg1995petrov <oleg1995petrov@yandex.by>
2021-10-25 17:59:57 +0300

    Update infrastructure

--- locals.tf
+++ locals.tf
@@ -25,7 +25,7 @@ locals {
     td          = merge(var.common_tags, { Name = "Task definition" })
   }
 
-  name_prefises = {
+  name_prefixes = {
     lb   = "${lower(var.common_tags.Project)}"
     lbtg = "${lower(var.common_tags.Project)}"
     lbsg = "${lower(var.common_tags.Project)}-${lower(var.common_tags.Environment)}-"
